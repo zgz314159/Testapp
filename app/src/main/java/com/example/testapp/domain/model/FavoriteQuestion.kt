4984c5ca005edd88714f338547cb5ccf9687797e
zgz314159 <zgz314159@gmail.com>
2025-07-05 18:45:49 +0800

    更新于 2025-07-05 18-45

--- app/src/main/java/com/example/testapp/domain/model/FavoriteQuestion.kt
+++ app/src/main/java/com/example/testapp/domain/model/FavoriteQuestion.kt
@@ -1,5 +1,8 @@
 package com.example.testapp.domain.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class FavoriteQuestion(
     val question: Question,
     val addedTime: Long = System.currentTimeMillis()
